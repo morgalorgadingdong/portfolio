217f17b56217424455c1944cdd4b9511009f0951
Morgan <morganfolz@gmail.com>
2023-06-29 20:16:35 -0700

    removed half of simmons listing photos

---
+++ src/routes/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+    export let currentPage
+
+    function changePage(page) {
+        currentPage = page
+        console.log(page)
+    }
+</script>
+
+
+<header class="col-12 d-flex justify-content-center flex-wrap">
+    <h1 class="col-12 text-center">Morgan Folz</h1>
+    <ul class="col-12 d-flex justify-content-between">
+        <li on:click={() => changePage('about')}>About</li>
+        <li on:click={() => changePage('projects')}>Projects</li>
+        <li on:click={() => changePage('resume')}>Resume</li>
+    </ul>
+</header>
+
+<style>
+    li {
+        list-style: none;
+    }
+</style>
